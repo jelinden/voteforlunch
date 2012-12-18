6c43d25388417c1a141a34bad3abcd98d43a9f12
Jouni Linden <jouni.linden@gmail.com>
2012-12-18 18:49:24 +0200

    Removed ; characters from js files.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ This is an exercise to make an application to vote for a lunch place.
 
 It's made with Redis, Spring-data and Backbone.js.
 
-More to come ...
+1. Install Redis (http://www.redis.io/download)
+2. Fetch the project and run it. mvn jetty:run
+3. Hope for the best (http://localhost:8080)
--- src/main/webapp/resources/js/user.js
+++ src/main/webapp/resources/js/user.js
@@ -1,15 +1,17 @@
-var User = Backbone.Model.extend({});
+var User = Backbone.Model.extend({})
 
 var UserStore = Backbone.Collection.extend({
   model: User,
   url: 'http://localhost:8080/voteforlunch/user'
-});
+})
 
-var user = new UserStore;
+var user = new UserStore
 
 var UserView = Backbone.View.extend({
   initialize: function() {
-    user.fetch({ success: function() { view.render(); } });
+    user.fetch({ success: function() { 
+    	view.render() 
+    } })
   },
 	
   events: { 
@@ -18,26 +20,31 @@ var UserView = Backbone.View.extend({
   },
 
   handleNewMessage: function(data) {
-	user.create({userName: $('#newuser').val()});
-    $('#newuser').val('');
+	user.create({userName: $('#newuser').val()})
+    $('#newuser').val('')
   },
 
   render: function() {
     var data = user.map(function(message) {
-    	return message.get('userName');
-    });
-    var result = data.reduce(function(memo,str) { return memo + '<div>' + str + '</div>'; }, '');
-    $("#users").html(result);
-    return this;
+    	return message.get('userName')
+    })
+    var result = data.reduce(function(json,str) { 
+    	return json + '<div>' + str + '</div>' 
+    }, '')
+    $("#users").html(result)
+    return this
   }
 
-});
+})
 
 user.bind('reset', function(message) {
-  user.fetch({ success: function() { view.render(); }});
-});
+  user.fetch({ success: function() { 
+	  view.render()
+  	}
+  })
+})
 
 var view = new UserView({
   el: $('#userTemplate')
-});
+})
 
